a64951d6021c93bed5224f9d2349a5d28bd04e02
Naresh Choudhary <nareshchoudhary1088@gmail.com>
2026-07-25 10:43:12 +0530

    Revert base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,16 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kiran-steels-websites/',
   server: {
     host: true,
     port: 1088,
-    strictPort: true, 
+    strictPort: true,
     hmr: {
       overlay: false,
     },
   },
   plugins: [
-    react(), 
+    react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
