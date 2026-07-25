bdcef825f7ca1b224f40dc31dd6cab25eca17d11
Naresh Choudhary <nareshchoudhary1088@gmail.com>
2026-07-25 10:25:15 +0530

    Fix GitHub pages build and add base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/kiran-steels-websites/',
   server: {
     host: true,
     port: 1088,
